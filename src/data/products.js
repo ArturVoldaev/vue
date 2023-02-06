01f70727b6267565797f2cae58bcd820e09ed2b1
fishka9044 <johndoe@example.comfishka9044@yandex.ru>
2023-02-07 00:04:54 +0300

    сделал 4.4

--- src/data/products.js
+++ src/data/products.js
@@ -1,77 +1,87 @@
 export default [
   {
-    title: 'Холодильник Candy CCRN 6200W',
+    id: 1,
+    title: "Холодильник Candy CCRN 6200W",
     price: 33690,
-    image: 'img/ice.webp'
+    image: "img/ice.webp",
   },
   {
-    title: 'Фен DEXP HD-1000',
+    id: 2,
+    title: "Фен DEXP HD-1000",
     price: 690,
-    image: 'img/heirdryDexp.webp'
+    image: "img/heirdryDexp.webp",
   },
   {
-    title: 'Радионяня Motorola MBP16',
+    id: 3,
+    title: "Радионяня Motorola MBP16",
     price: 3690,
-    image: 'img/radio.jpg'
+    image: "img/radio.jpg",
   },
   {
-    title: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
+    id: 4,
+    title: "Ультразвуковая зубная щётка Playbrush Smart Sonic",
     price: 5690,
-    image: 'img/toothbrush.jpg'
+    image: "img/toothbrush.jpg",
   },
   {
-    title: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
+    id: 5,
+    title: "Смартфон Xiaomi Mi Mix 3 6/128GB",
     price: 21690,
-    image: 'img/phone.jpg'
-  },  
+    image: "img/phone.jpg",
+  },
   {
-    title: 'Электроскейт Razor Cruiser',
+    id: 6,
+    title: "Электроскейт Razor Cruiser",
     price: 24690,
-    image: 'img/board.jpg'
+    image: "img/board.jpg",
   },
   {
-    title: 'Смартфон Xiaomi Mi A3 4/64GB Android One',
+    id: 7,
+    title: "Смартфон Xiaomi Mi A3 4/64GB Android One",
     price: 14690,
-    image: 'img/phone-2.jpg'
+    image: "img/phone-2.jpg",
   },
   {
-    title: 'Смартфон Xiaomi Redmi 6/128GB',
+    id: 8,
+    title: "Смартфон Xiaomi Redmi 6/128GB",
     price: 8690,
-    image: 'img/phone-3.jpg'
+    image: "img/phone-3.jpg",
   },
   {
-    title: 'Электрический дрифт-карт Razor Crazy Cart',
+    id: 9,
+    title: "Электрический дрифт-карт Razor Crazy Cart",
     price: 39690,
-    image: 'img/bicycle.jpg'
+    image: "img/bicycle.jpg",
   },
   {
-    title: 'Гироскутер Razor Hovertrax 2.0',
+    id: 10,
+    title: "Гироскутер Razor Hovertrax 2.0",
     price: 34690,
-    image: 'img/wheels.jpg'
+    image: "img/wheels.jpg",
   },
   {
-    title: 'Детский трюковой самокат Razor Grom',
+    id: 11,
+    title: "Детский трюковой самокат Razor Grom",
     price: 4690,
-    image: 'img/scooter.jpg'
+    image: "img/scooter.jpg",
   },
   {
-    title: 'Роллерсёрф Razor RipStik Air Pro',
+    id: 12,
+    title: "Роллерсёрф Razor RipStik Air Pro",
     price: 6690,
-    image: 'img/ripstik.jpg'
+    image: "img/ripstik.jpg",
   },
   {
-    title: 'Наушники AirPods с беспроводным зарядным футляром',
+    id: 13,
+    title: "Наушники AirPods с беспроводным зарядным футляром",
     price: 16690,
-    image: 'img/airpods.jpg'
+    image: "img/airpods.jpg",
   },
+  { id: 14, title: "Наушники Sony", price: 30690, image: "img/headphones.jpg" },
   {
-    title: 'Наушники Sony',
-    price: 30690,
-    image: 'img/headphones.jpg'
-  },
-  {
-    title: 'Радионяня Motorola MBP16',
+    id: 15,
+    title: "Радионяня Motorola MBP16",
     price: 3690,
-    image: 'img/radio.jpg'
+    image: "img/radio.jpg",
   },
-]
+];
